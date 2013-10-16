060cf88979b3ab97bc2c017e288179070d2a11ef
ragsdalewn <ragsdalewn@goldmail.etsu.edu>
2013-10-15 22:48:02 -0400

    Starting Point

---
+++ Zoo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zoo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }//end Index
+
+        public ActionResult ViewEmployeeSchedule()
+        {
+
+            return View();
+        }//end ViewEmployeeSchedule
+
+    }//end controller
+}//end namespace
+
+     
